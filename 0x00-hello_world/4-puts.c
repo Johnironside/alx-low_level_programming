9009ee1fbc6ae856f381faa96ef34affb61f4292
John_ironside <johnogundeji001@gmail.com>
2022-10-13 07:54:19 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main (void)
+/**
+* main - It is responsible for starting and terminating a program
+* return - 0
+*/
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
