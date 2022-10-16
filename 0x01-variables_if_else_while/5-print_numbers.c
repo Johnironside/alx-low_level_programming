c55e376a89332fbe049f4422c82d55b1ec22e739
John_ironside <johnogundeji001@gmail.com>
2022-10-16 16:12:27 -0700

    Figures from zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints the figures "0123456789"
+* Return: 0 if executed properly, non-zero otherwise
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+	printf("%d", i);
+	i++;
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
