2b21cdd4d3acfae827cbea329fad9b715eee7921
John_ironside <johnogundeji001@gmail.com>
2022-10-16 13:51:52 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabets 'a' - 'z'
+* Return: 0 when executed properly, and non-zero otherwise
+*/
+
+int main(void)
+{
+	char ch = 'a'
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+
+	return (0);
+
+}
